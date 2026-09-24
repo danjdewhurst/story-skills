a472102548e91de1d7fb54ce8f9be5f779498d7e
Dan <danjdewhurst@users.noreply.github.com>
2026-09-24 20:24:20 +0000

    chore: release 0.8.1

--- skills/story-maintenance/scripts/story.js
+++ skills/story-maintenance/scripts/story.js
@@ -7163,7 +7163,7 @@ function reportResult(io, result, successMessage, failureMessage) {
 }
 
 // src/version.js
-var VERSION = "0.8.0";
+var VERSION = "0.8.1";
 
 // src/cli.js
 var COMMANDS_BY_NAME = new Map(COMMANDS.map((command) => [command.name, command]));
